e3c23ce3eb106eeec70a41e62faaed951c40744b
dipak-95 <dipakladani95@gmail.com>
2026-01-17 13:27:46 +0530

    Update API URL to sdpearl.cloud

--- admin/src/lib/config.ts
+++ admin/src/lib/config.ts
@@ -1,3 +1,3 @@
 // Points to Nginx Proxy Path (Port 80) to bypass Firewall issues
-export const API_BASE_URL = 'http://157.173.218.106/api';
-export const UPLOAD_BASE_URL = 'http://157.173.218.106/api';
+export const API_BASE_URL = 'http://sdpearl.cloud/api';
+export const UPLOAD_BASE_URL = 'http://sdpearl.cloud/api';
